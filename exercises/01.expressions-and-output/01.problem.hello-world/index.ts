da7be534118a4b16a28f46a776d9a8ddac52abeb
fofoarellano808 <adolfoarellano.df@gmail.com>
2026-03-24 05:02:15 +0000

    finished

--- exercises/01.expressions-and-output/01.problem.hello-world/index.ts
+++ exercises/01.expressions-and-output/01.problem.hello-world/index.ts
@@ -2,5 +2,8 @@
 // Let's print some messages to the console!
 
 // 🐨 Use console.log() to print "Hello, World!"
+console.log('Hello, World!');
+
 
 // 🐨 Use console.log() to print your name (as a string)
+console.log('Adolfo Arellano');
